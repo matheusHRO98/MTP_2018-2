6c1666607474aca47038316675e1873038461f0d
matheusHRO98 <42752303+matheusHRO98@users.noreply.github.com>
2018-12-12 14:49:58 -0200

    Update p5.c

--- p5.c
+++ p5.c
@@ -4,23 +4,74 @@
 
 #include <stdio.h>
 
-int A(int,int );
-int main(){
-	int m,n;
-	printf("Insira valor para m e n , de espaco:\n");
-	scanf("%d%d",&m,&n);
-	printf("%d",A(m,n));		
-	return 0;
-	}
-	
-int A(int m,int n)
-	{
-		if(m== 0)
-		return n+1;
-		else if(n==0 && m>0)
-		return A(m-1,1);
-		else
-        return A(m - 1, A(m, n - 1));
-		}
-		
+typedef
+unsigned long long int
+Bytes8;
+typedef
+struct LCG { Bytes8 a, c, m, rand_max, atual; }
+LCG;
 
+void semente(LCG * r, Bytes8 seed) {
+// constantes do POSIX [de]rand48, glibc [de]rand48[_r]
+// ULL transforma a constante ’int’ em ’unsigned long long int’
+r->a = 0x5DEECE66DULL;
+r->c = 11ULL;
+r->m = (1ULL << 48);
+r->rand_max = r->m - 1;
+r->atual = seed;
+}
+
+Bytes8 lcg_rand(LCG * r) {
+r->atual = (r->a * r->atual + r->c) % r->m;
+return r->atual;
+}
+
+double lcg_rand_01(LCG * r) {
+return ((double) lcg_rand(r))/(r->rand_max);
+}
+
+void gera_numeros(float * vetor, int tam,float min, float max, LCG * r) {
+int i;
+for(i = 0; i < tam; i++)
+vetor[i] = (max-min)*lcg_rand_01(r) + min;
+}
+
+float soma(float *v_ini,float *v_fim){
+       return (v_ini == v_fim+1)? 0 : *v_ini + soma(v_ini+1,v_fim);
+}
+	    
+float produto(float *v_ini,float *v_fim){
+	return (v_ini == v_fim+1)? 1 : *v_ini * produto(v_ini+1,v_fim);
+}
+
+int main(){
+	LCG random;
+    semente(&random,123456);
+    int op=0;
+    float vet[50];
+    int N = 50;
+    gera_numeros(vet,N, 0.5, 1.5, &random);
+    printf("Este programa realiza calculos com 50 numeros gerados aleatoriamente.");
+	do{printf("\nEscolha a operacao a ser realizada:\n[1]Somatorio \n[2]Produtorio.\n[3]Sair\n");
+	scanf("%d",&op);
+	switch(op){
+		case 1:
+		
+		printf("\nSoma : %f", soma( &vet[0],&vet[0] + N-1 ) );
+		break;
+	
+		case 2:
+		
+		printf("\nProduto: %g", produto( &vet[0], &vet[0] + N-1) );
+		break;
+		
+		default:
+ 	    {
+ 		if(op!=3)
+ 		printf("Operacao invalida!\n");
+ 		break;
+    }
+  }	
+}while(op!=3);
+return 0 ;
+}
