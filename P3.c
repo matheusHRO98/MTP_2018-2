f6b8c777d1dd9288c12c12248e7f95bb1223509b
matheusHRO98 <42752303+matheusHRO98@users.noreply.github.com>
2018-12-12 14:48:30 -0200

    Update P3.c

--- P3.c
+++ P3.c
@@ -3,94 +3,20 @@
 
 #include <stdio.h>
 
-void ConverterBinario(unsigned n)
-{
-	int i = 0;
-	unsigned mask = 1 << 31;
-	printf("%d (", n);
-	for (i = 0; i <= 32; i++)
+int main(){
+	int i,cont=0;
+	char numero[256];
+	printf("\nInsira um numero:");
+	scanf("%s",numero);
+	for(i=0;numero[i]!='\0'; i++)
 	{
-		char c = n & mask ? '1' : '0';
-		n <<= 1;
-		printf("%c", c);
+       if(numero[i] >='0' && numero[i] <='9' ) 
+       {
+       	printf("%d",numero[i]-'0');
+       	cont++;
+       }
 	}
-	printf(")");
-}
-
-int main()
-{
-	int opcao = 0, num1, num2;
-	printf("CALCULADORA, digite 0 para sair.\n\n");
-	while (1)
-	{
-		printf("\nDigite a opcao: ");
-		scanf("%d", &opcao);
-
-		if (opcao == 0)
-			break;
-
-		switch (opcao)
-		{
-		case 1:
-			printf("\nDigite um numero: ");
-			fflush(stdin);
-			scanf("%d", &num1);
-
-			printf("NOT ");	ConverterBinario(~num1); printf(" : "); ConverterBinario(num1);
-			break;
-
-		case 2:
-			printf("\nDigite um numero: ");
-			scanf("%d", &num1);
-			printf("\nDigite outro numero: ");
-			scanf("%d", &num2);
-
-			ConverterBinario(num1); printf(" AND "); ConverterBinario(num2);
-			printf(" : "); ConverterBinario(num1&num2);
-			break;
-
-		case 3:
-			printf("\nDigite um numero: ");
-			scanf("%d", &num1);
-			printf("\nDigite outro numero: ");
-			scanf("%d", &num2);
-
-			ConverterBinario(num1); printf(" OR "); ConverterBinario(num2);
-			printf(" : "); ConverterBinario(num1 | num2);
-			break;
-
-		case 4:
-			printf("\nDigite um numero: ");
-			scanf("%d", &num1);
-			printf("\nDigite outro numero: ");
-			scanf("%d", &num2);
-
-			ConverterBinario(num1); printf(" XOR "); ConverterBinario(num2);
-			printf(" : "); ConverterBinario(num1 ^ num2);
-			break;
-
-		case 5:
-			printf("\nDigite um numero: ");
-			scanf("%d", &num1);
-			printf("\nDigite outro numero: ");
-			scanf("%d", &num2);
-
-			ConverterBinario(num1); printf(" >> "); ConverterBinario(num2);
-			printf(" : "); ConverterBinario(num1 >> num2);
-			break;
-
-		case 6:
-			printf("\nDigite um numero: ");
-			scanf("%d", &num1);
-			printf("\nDigite outro numero: ");
-			scanf("%d", &num2);
-
-			ConverterBinario(num1); printf(" << "); ConverterBinario(num2);
-			printf(" : "); ConverterBinario(num1 << num2);
-			break;
-		default:
-			break;
-		}
-	}
-}
+	if(cont==0)
+	printf("0");
+	return 0;}
 
